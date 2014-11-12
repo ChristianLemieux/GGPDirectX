cbe7fbc06cac4afcb5c9552ebfae8e4445d2bd6d
Alyssa Abraham <ala3497@rit.edu>
2014-11-11 19:29:40 -0500

    StateManager Class

---
+++ DirectX11_Starter/DirectX11_Starter/DirectX11_Starter/StateManager.h
@@ -0,0 +1,16 @@
+#ifndef _STATEMANAGER_H
+#define _STATEMANAGER_H
+
+#include <string>
+using namespace std;
+class StateManager{
+public:
+	StateManager();
+	wchar_t* changeState();
+private:
+protected:
+	wchar_t* states[6];
+	int adj[6][6];
+	int currentState;
+};
+#endif
