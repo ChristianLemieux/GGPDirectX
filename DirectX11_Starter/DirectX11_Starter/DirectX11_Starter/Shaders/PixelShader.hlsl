83c777592fb5b9f62858ace514a31aff5d389826
jrunks <runkel.jonathan@gmail.com>
2014-11-12 21:27:53 -0500

    New

--- DirectX11_Starter/DirectX11_Starter/DirectX11_Starter/Shaders/PixelShader.hlsl
+++ DirectX11_Starter/DirectX11_Starter/DirectX11_Starter/Shaders/PixelShader.hlsl
@@ -10,6 +10,85 @@ struct VertexToPixel
 	float2 uv			: TEXCOORD1;
 };
 
+// for lighting
+struct DirectionalLight
+{
+	float4 Ambient;
+	float4 Diffuse;
+	float4 Specular;
+	float3 Direction;
+	float pad;
+};
+
+struct PointLight
+{
+	float4 Ambient;
+	float4 Diffuse;
+	float4 Specular;
+
+	float3 Position;
+	float Range;
+
+	float3 Att;
+	float pad;
+};
+
+struct SpotLight
+{
+	float4 Ambient;
+	float4 Diffuse;
+	float4 Specular;
+
+	float3 Position;
+	float Range;
+
+	float3 Direction;
+	float Spot;
+
+	float3 Att;
+	float pad;
+};
+
+cbuffer cbPerFrame
+{
+	DirectionalLight gDirLight;
+	PointLight gPointLight;
+	SpotLight gSpotLight;
+	float3 gEyePosW;
+};
+
+void ComputeDirectionalLight(DirectionalLight L,
+	float3 normal, float3 toEye,
+	out float4 ambient,
+	out float4 diffuse,
+	out float4 spec)
+{
+	// Initialize outputs.
+	ambient = float4(0.0f, 0.0f, 0.0f, 0.0f);
+	diffuse = float4(0.0f, 0.0f, 0.0f, 0.0f);
+	spec = float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+	// The light vector aims opposite the direction the light rays travel.
+	float3 lightVec = -L.Direction;
+
+		// Add ambient term.
+		ambient = L.Ambient;
+
+	// Add diffuse and specular term, provided the surface is in
+	// the line of site of the light.
+
+	float diffuseFactor = dot(lightVec, normal);
+
+	// Flatten to avoid dynamic branching.
+	[flatten]
+	if (diffuseFactor > 0.0f)
+	{
+		float3 v = reflect(-lightVec, normal);
+
+		diffuse = diffuseFactor * L.Diffuse;
+	}
+}
+
 // Entry point for this pixel shader
 float4 main(VertexToPixel input) : SV_TARGET
 {
